3e9ab60ba6ad5e2dfa520f0eb9ea7e3a5045b401
Anas-1-Mohd <mohammad.anas@quadbtech.com>
2024-05-08 12:56:51 +0530

    Add Backend directrory structrue

--- src/icp_dex_backend/src/lib.rs
+++ src/icp_dex_backend/src/lib.rs
@@ -1,8 +1,35 @@
-use ic_cdk::export_candid;
+pub mod api;
+mod memory;
+mod state_handler;
+mod types;
+mod upgrade;
+mod user;
+use candid::Principal;
+use ic_cdk::{caller, export_candid, post_upgrade, pre_upgrade};
+use ic_cdk_macros::{query, update};
+use ic_stable_structures::StableBTreeMap;
+use memory::Memory;
+use serde::{Deserialize, Serialize};
+use state_handler::State;
+use std::cell::RefCell;
 
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+thread_local! {
+    static STATE: RefCell<State> = RefCell::new(State::new());
+}
+
+pub fn with_state<R>(f: impl FnOnce(&mut State) -> R) -> R {
+    STATE.with(|cell| f(&mut cell.borrow_mut()))
+}
+
+// PreUpgrade and PostUpgrade for stable memory
+#[pre_upgrade]
+fn pre_upgrade() {
+    upgrade::pre_upgrade();
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    upgrade::post_upgrade();
 }
 
 export_candid!();
