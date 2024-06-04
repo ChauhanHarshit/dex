6e6345698f47c66e5eea8f15d363a876276e2d3c
jhoncarter7 <rainikhil044@gmail.com>
2024-06-04 12:45:53 +0530

    fix: align footer

--- src/icp_dex_frontend/src/components/footer/Footer.jsx
+++ src/icp_dex_frontend/src/components/footer/Footer.jsx
@@ -6,20 +6,20 @@ import { BsDiscord, BsYoutube } from 'react-icons/bs';
 import NorthEastIcon from '@mui/icons-material/NorthEast';
 function Footer() {
     return (
-        <div>
+        <div className=''>
             <hr className='  border-gray-700 ' />
-            <div className='grid md:grid-cols-4 grid-cols-2 justify-end  gap-y-12 text-white px-4 sm:px-24 md:pr-12  h-screen items-center' >
+            <div className='grid md:grid-cols-4 grid-cols-2  m-0 gap-y-12 text-white px-4 sm:px-24 md:pr-12   translate-y-[50%] pb-28' >
 
                 <div className='space-y-4'>
-                    <h1>Value swap.io</h1>
-                    <ul className='flex flex-col gap-y-2'>
+                    <h1 className='text-lg'>Value swap.io</h1>
+                    <ul className='flex flex-col gap-y-2 cursor-pointer'>
                         <li>Home</li>
                         <li>Build</li>
                     </ul>
                 </div>
                 <div className='space-y-4'>
-                    <h1>Learn</h1>
-                    <ul className='flex flex-col gap-y-2'>
+                    <h1  className='text-lg'>Learn</h1>
+                    <ul className='flex flex-col gap-y-2 cursor-pointer'>
                         <li>Docs <NorthEastIcon style={{ fontSize: '1rem', paddingBottom: "1px" }} /></li>
                         <li>Risk <NorthEastIcon style={{ fontSize: '1rem', paddingBottom: "1px" }} /></li>
                         <li>WhitePaper <NorthEastIcon style={{ fontSize: '1rem', paddingBottom: "1px" }} /></li>
@@ -28,16 +28,16 @@ function Footer() {
                     </ul>
                 </div>
                 <div className='space-y-4'>
-                    <h1>Ecosystem</h1>
-                    <ul className='flex flex-col gap-y-2'>
+                    <h1  className='text-lg'>Ecosystem</h1>
+                    <ul className='flex flex-col gap-y-2 cursor-pointer'>
                         <li>Forums <NorthEastIcon style={{ fontSize: '1rem', paddingBottom: "1px" }} /></li>
                         <li>Grants <NorthEastIcon style={{ fontSize: '1rem', paddingBottom: "1px" }} /></li>
                         <li>Brands assets <NorthEastIcon style={{ fontSize: '1rem', paddingBottom: "1px" }} /> </li>
                     </ul>
                 </div>
                 <div className='space-y-4'>
-                    <h1>community</h1>
-                    <div className='grid grid-cols-3 md:grid-cols-4 gap-y-4 text-center md:gap-y-5 pt-4'>
+                    <h1  className='text-lg'>community</h1>
+                    <div className='grid grid-cols-3 md:grid-cols-4 gap-y-4 text-center md:gap-y-5 pt-4 cursor-pointer'>
 
                         <TwitterIcon />
                         <GitHubIcon />
